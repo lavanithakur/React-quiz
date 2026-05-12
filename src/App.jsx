8fdc40ef899c09773af47243acc34398b54af7d0
lavanithakur <lavanithakur@gmail.com>
2026-05-12 10:34:03 +0530

    setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold text-blue-500">
+  Hello Tailwind!
+</h1>
+    </div>
+  )
+}
+
+export default App
